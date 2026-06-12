02ac6e8adb08b72e2471f9f71c803a40f4d2da6d
SoCooWhat <770878695@qq.com>
2026-06-12 20:29:35 +0800

    fix: remove deprecated buildActivity config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 开发服务器优化
-  devIndicators: {
-    buildActivity: true,
-    buildActivityPosition: 'bottom-right',
-  },
 };
 
 export default nextConfig;
